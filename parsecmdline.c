504a505b01d4cfa9319f7916e29d71ef97d8259c
PDutriez <42210994+PDutriez@users.noreply.github.com>
2019-03-17 16:39:29 -0300

    Update parsecmdline.c

--- parsecmdline.c
+++ parsecmdline.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
-#define EXITVALUE -1
+#include "parsecmdline.h"
 
-int parseCmdLine(int argc, char* argv[], int (*parseCallback) (char*, char*, void*), void *userData){
-	int argCount=2;/*me salteo el nombre del archivo y el primer parametro*/
-	int result=1; /*lo que devuleve parseCallback, flag siempre prendido salvo cuando parseCallback devuelve 0*/
-	int unckeckedV=0; /*flag se prende en el caso que haya un key sin value*/
-	int cantArgs=1; /*cantidad de argumentos totales*/
-	char * value; /*guarda value valido para llamar a parseCallback*/
-	char * key; /*guarda key valido para llamar a parseCallBack*/
+int parseCmdLine(int argc, char* argv[],pCallback parseCallBack, void *userData)
+{
+	int argCount=1;/*me salteo el nombre del archivo y el primer parametro*/
+	int cantArgs=0; /*cantidad de argumentos totales*/
 	 /*chequear caso que no haya nada!!!*/
-	while(argCount<argc && !unckeckedV && result){ /*ciclo corta si encuentra un error de cualquier tipo*/
-       if(*(argv+argCount)=='-'){ 
-            if(argc==argCount){  /*si key y value son validos, los guarda*/
-            	unckeckedV=1;
-            }
-            key=*(argv+argCount); /*faltaria sacar valor de -*/
-            value=*(argv+argCount+1);
-            argv++;
-        }
-       else{
-       	   value=*(argv+argCount); /*si es un parametro*/
-       	   key=NULL;
-       	   }
-       	cantArgs++;
-       result=parseCallback(key, value, userData);
-   }
-   if (result && !unckeckedV){
-   	return cantArgs;
-   }
-   return EXITVALUE;
+	for(;argCount<argc && cantArgs!=EXITVALUE ;++argCount)
+	{ //ciclo corta si encuentra un error de cualquier tipo
+			printf("%s\n",argv[argCount]);
+			if(argv[argCount][0]=='-')//CASO OPCION
+			{
+					if(argv[argCount][1] == '\0')	//Analiza si el caracter siguiente al guion es vacio)
+					{
+							cantArgs=EXITVALUE;
+					}
+		      else if(argCount==(argc-1))  //si key y value son validos, los guarda
+		      {
+							cantArgs=EXITVALUE;
+					}
+					else if(!((*parseCallBack)(argv[argCount]+1, argv[argCount+1], userData)))	//Analizo Callback
+					{
+							cantArgs=EXITVALUE;
+					}
+					else
+					{
+								++argCount;//Todo OK -> saltear el valor
+					}
+			}
+	    else//CASO PARAMETRO
+			{
+				if(!((*parseCallBack)(NULL, argv[argCount], userData)))	//Analizo Callback
+					cantArgs=EXITVALUE;
+			}
+			if(cantArgs!=EXITVALUE)
+					++cantArgs;//Si todo fue valido se aumenta el contador de argumentos
+  }
+
+   return cantArgs;
 }
